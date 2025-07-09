ef634043c4483be38243174245493c0b34cb929e
g4rb4g3 <g4rb4g3@gmail.com>
2025-07-09 13:54:56 +0200

    replace deprecated methods

--- packages/react-native-carplay/android/src/main/java/org/birkir/carplay/parser/RCTMessageTemplate.kt
+++ packages/react-native-carplay/android/src/main/java/org/birkir/carplay/parser/RCTMessageTemplate.kt
@@ -1,6 +1,7 @@
 package org.birkir.carplay.parser
 
 import androidx.car.app.CarContext
+import androidx.car.app.model.Header
 import androidx.car.app.model.MessageTemplate
 import com.facebook.react.bridge.ReadableMap
 import org.birkir.carplay.screens.CarScreenContext
@@ -13,10 +14,21 @@ class RCTMessageTemplate(
     val message = props.getString("message") ?: "No message"
     val messageText = Parser.parseCarText(message, props)
     return MessageTemplate.Builder(messageText).apply {
-      props.getArray("actions")?.let { setActionStrip(parseActionStrip(it)) }
-      props.getMap("headerAction")?.let { setHeaderAction(Parser.parseAction(it, context, eventEmitter)) }
+      props.getArray("actions")?.let {
+        for (i in 0 until it.size()) {
+          val map = it.getMap(i)
+          addAction(Parser.parseAction(map, context, carScreenContext.eventEmitter))
+        }
+      }
+      setHeader(Header.Builder().apply {
+        props.getString("title")?.let {
+          setTitle(it)
+        }
+        props.getMap("headerAction")?.let {
+          setStartHeaderAction(Parser.parseAction(it, context, eventEmitter))
+        }
+      }.build())
       props.getMap("image")?.let { setIcon(Parser.parseCarIcon(it, context)) }
-      props.getString("title")?.let { setTitle(it) }
       props.getString("debugMessage")?.let { setDebugMessage(it) }
       setLoading(props.isLoading())
     }.build()
