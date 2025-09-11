91d8ed3716276af0fd4f47c03c286d8f17a9fc46
g4rb4g3 <g4rb4g3@gmail.com>
2025-09-11 12:35:38 +0200

    fix: Cannot read property 'emitter' of undefined

--- packages/react-native-carplay/src/CarPlayHeadlessJsTask.ts
+++ packages/react-native-carplay/src/CarPlayHeadlessJsTask.ts
@@ -1,15 +1,18 @@
 import type { EmitterSubscription, Task, TaskProvider } from 'react-native';
-import { AppRegistry, Platform } from 'react-native';
-import { CarPlay } from './CarPlay';
+import { AppRegistry, NativeEventEmitter, NativeModules, Platform } from 'react-native';
+import { InternalCarPlay } from './interfaces/InternalCarPlay';
 
 // this headless task is required on Android (Auto) to make sure timers are working fine when screen is off
+
+const { RNCarPlay } = NativeModules as { RNCarPlay: InternalCarPlay };
+const emitter = new NativeEventEmitter(RNCarPlay);
 
 const headlessTask: TaskProvider = (): Task => _ =>
   new Promise((resolve, reject) => {
     let subscription: EmitterSubscription | null = null;
 
     try {
-      subscription = CarPlay.emitter.addListener('didDisconnect', () => {
+      subscription = emitter.addListener('didDisconnect', () => {
         try {
           subscription?.remove();
           resolve();
